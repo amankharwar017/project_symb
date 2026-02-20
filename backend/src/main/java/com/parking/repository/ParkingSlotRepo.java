660fa19755c1ed40e94b46ba222066c8c028461e
Aman Kharwar <amankharwar755@gmail.com>
2026-02-20 20:59:53 +0530

    change backend

--- backend/src/main/java/com/parking/repository/ParkingSlotRepo.java
+++ backend/src/main/java/com/parking/repository/ParkingSlotRepo.java
@@ -3,6 +3,8 @@ package com.parking.repository;
 
 import com.parking.entity.ParkingSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ParkingSlotRepo extends JpaRepository<ParkingSlot, Integer> {
 }
