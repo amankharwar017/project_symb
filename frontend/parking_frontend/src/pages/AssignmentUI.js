34cd942730e813522d8729c9d4fee5c8d32796f0
Aman Kharwar <amankharwar755@gmail.com>
2026-02-24 13:00:47 +0530

    Remove cloudflare and use AWS backend

--- frontend/parking_frontend/src/pages/AssignmentUI.js
+++ frontend/parking_frontend/src/pages/AssignmentUI.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API = "http://localhost:8080/api/slots";
+const API = "https://Parking-backend-24-env.eba-ky3ymcix.ap-southeast-2.elasticbeanstalk.com/api/slots";
 
 export default function AssignmentUI() {
   const [slots, setSlots] = useState([]);
